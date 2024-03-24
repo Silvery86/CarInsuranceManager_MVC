bc880951223ab88ffa21f00ba3c599ca253171dc
giang8692@gmail.com <giang8692@gmail.com>
2024-03-24 16:28:40 +0700

    Add Vehicle controller and some sample data

---
+++ CarInsuranceManagerWeb/Controllers/VehicleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarInsuranceManagerWeb.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CarInsuranceManagerWeb/Models/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarInsuranceManagerWeb.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        [Required]
+        public required string OwnerName { get; set; }
+        [Required]
+        public required string Model { get; set; }
+        [Required]
+        public required string Version { get; set; }
+        [Required]
+        public int Rate { get; set; }
+        [Required]
+        public required string BodyNumber { get; set; }
+        [Required]
+        public required string EngineNumber { get; set; }
+        [Required]
+        public required string Number { get; set; }
+
+    }
+}
