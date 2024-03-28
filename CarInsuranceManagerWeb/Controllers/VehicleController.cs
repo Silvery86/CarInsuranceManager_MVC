b4e6431e390acb07d74d697db6bd4085ca77603d
giang8692@gmail.com <giang8692@gmail.com>
2024-03-28 20:33:06 +0700

    Update: Function to add vehicle data to DB

--- CarInsuranceManagerWeb/Controllers/VehicleController.cs
+++ CarInsuranceManagerWeb/Controllers/VehicleController.cs
@@ -1,12 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarInsuranceManagerWeb.Data;
+using CarInsuranceManagerWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CarInsuranceManagerWeb.Controllers
 {
     public class VehicleController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public VehicleController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
         public IActionResult Index()
         {
+            List<Vehicle> vehicles = _db.Vehicles.ToList();
+            return View(vehicles);
+        }
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                // Check uniqueness for BodyNumber, EngineNumber, and Number
+                if (_db.Vehicles.Any(v => v.Number == vehicle.Number ||
+                                           v.BodyNumber == vehicle.BodyNumber ||
+                                           v.EngineNumber == vehicle.EngineNumber))
+                {
+                    if (_db.Vehicles.Any(v => v.Number == vehicle.Number))
+                    {
+                        ModelState.AddModelError(nameof(vehicle.Number), "A vehicle with this number already exists.");
+                    }
+                    if (_db.Vehicles.Any(v => v.BodyNumber == vehicle.BodyNumber))
+                    {
+                        ModelState.AddModelError(nameof(vehicle.BodyNumber), "A vehicle with this body number already exists.");
+                    }
+                    if (_db.Vehicles.Any(v => v.EngineNumber == vehicle.EngineNumber))
+                    {
+                        ModelState.AddModelError(nameof(vehicle.EngineNumber), "A vehicle with this engine number already exists.");
+                    }
+
+                    return View(vehicle); // Return the view to display validation errors
+                }
+                _db.Vehicles.Add(vehicle);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+
         }
     }
 }
--- CarInsuranceManagerWeb/Models/Vehicle.cs
+++ CarInsuranceManagerWeb/Models/Vehicle.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarInsuranceManagerWeb.Models
 {
@@ -7,21 +8,35 @@ namespace CarInsuranceManagerWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(50)]
+        [DisplayName("Vehicle Name")]
         public required string Name { get; set; }
         [Required]
+        [DisplayName("Owner Name")]
         public required string OwnerName { get; set; }
         [Required]
+        [DisplayName("Vehicle Model")]
         public required string Model { get; set; }
         [Required]
+        [DisplayName("Vehicle Version")]
         public required string Version { get; set; }
         [Required]
+        [Range(1, 100)]
+        [DisplayName("Vehicle Rate")]
         public int Rate { get; set; }
         [Required]
+        [MaxLength(25)]
+        [DisplayName("Vehicle Body Number")]
         public required string BodyNumber { get; set; }
         [Required]
+        [MaxLength(25)]
+        [DisplayName("Vehicle Engine Number")]
         public required string EngineNumber { get; set; }
         [Required]
+        [DisplayName("Vehicle Number")]
+        [StringLength(8)]
         public required string Number { get; set; }
+
 
     }
 }
